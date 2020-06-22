982748b05a7f852f1adf69dd1cab46a5cabd921a
Suraj0712 <sssapkal@wpi.edu>
2020-06-22 00:02:32 -0400

    Solved the quiz for adding Lidar points on the image plane

--- quiz/1_Lidar_to_Camera/src/project_lidar_to_camera.cpp
+++ quiz/1_Lidar_to_Camera/src/project_lidar_to_camera.cpp
@@ -49,12 +49,19 @@ void projectLidarToCamera2()
     cv::Mat Y(3,1,cv::DataType<double>::type);
     for(auto it=lidarPoints.begin(); it!=lidarPoints.end(); ++it) {
         // 1. Convert current Lidar point into homogeneous coordinates and store it in the 4D variable X.
+        X.at<double>(0,0) = it->x;
+        X.at<double>(1,0) = it->y;
+        X.at<double>(2,0) = it->z;
+        X.at<double>(3,0) = 1;
 
         // 2. Then, apply the projection equation as detailed in lesson 5.1 to map X onto the image plane of the camera. 
         // Store the result in Y.
+        Y= P_rect_00 * R_rect_00 * RT * X;
 
         // 3. Once this is done, transform Y back into Euclidean coordinates and store the result in the variable pt.
         cv::Point pt;
+        pt.x = Y.at<double>(0,0)/ Y.at<double>(2,0);
+        pt.y = Y.at<double>(1,0)/ Y.at<double>(2,0);
 
         float val = it->x;
         float maxVal = 20.0;
